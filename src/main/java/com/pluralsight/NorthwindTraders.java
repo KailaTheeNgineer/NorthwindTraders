b3c71aa3345f2dfad394826ff843fe49fa1f7f71
KailaTheeNgineer <kamcalpine717@gmail.com>
2024-06-11 10:05:49 -0400

    New screens

--- src/main/java/com/pluralsight/NorthwindTraders.java
+++ src/main/java/com/pluralsight/NorthwindTraders.java
@@ -1,40 +1,78 @@
 package com.pluralsight;
 
 import java.sql.*;
-
+import java.util.Scanner;
 
 
 public class NorthwindTraders {
+
+public static Scanner myScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         String password = args[0];
         String url = "jdbc:mysql://localhost:3306/northwind";
         String username = "root";
 
-        System.out.println("All Products by NorthWind: \n");
+        System.out.println("Select an Option:\n 1) Display All Products\n 2) Display All Customers\n 0) Exit");
 
-        String query = "SELECT * FROM Products";
-
+        String userInput = myScanner.nextLine();
 
         try {
 
-            Connection connection;
-            connection = DriverManager.getConnection(url, username, password);
-            PreparedStatement statement = connection.prepareStatement(query);
+            if (userInput.equalsIgnoreCase("1")) {
+                System.out.println("All Products: \n");
+                String allProductsQuery = "SELECT * FROM Products";
 
 
-            ResultSet results = statement.executeQuery();
+                Connection connection;
+                connection = DriverManager.getConnection(url, username, password);
+                PreparedStatement statement = connection.prepareStatement(allProductsQuery);
 
-            while(results.next()) {
-                System.out.println(results.getString("ProductName"));
+
+                ResultSet results = statement.executeQuery();
+
+                while (results.next()) {
+                    System.out.println(results.getString("ProductName"));
+                }
+
+                results.close();
+                statement.close();
+                connection.close();
+
+
+            } else if (userInput.equalsIgnoreCase("2")) {
+                System.out.println("All Customers: \n");
+                String allCustomersQuery = "SELECT * FROM Customers";
+
+
+                Connection connection;
+                connection = DriverManager.getConnection(url, username, password);
+                PreparedStatement statement = connection.prepareStatement(allCustomersQuery);
+
+
+                ResultSet results = statement.executeQuery();
+
+                while (results.next()) {
+                    System.out.println(results.getString("ContactName"));
+                }
+
+                results.close();
+                statement.close();
+                connection.close();
+
+
+            } else {
+                System.exit(0);
             }
 
-            results.close();
-            statement.close();
-            connection.close();
 
-            
         }
+
+
+
+
+
+
         
         catch (SQLException e) {
             e.printStackTrace();
